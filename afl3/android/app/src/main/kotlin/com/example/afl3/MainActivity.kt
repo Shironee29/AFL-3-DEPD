dafef04d86c4a5345c3c9cfbd852cdd8ba9dfe06
Shironee29 <153483750+Shironee29@users.noreply.github.com>
2025-12-07 14:09:46 +0700

    revisi 1

--- afl3/android/app/src/main/kotlin/com/example/afl3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.afl3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ afl3depd/android/app/src/main/kotlin/com/example/afl3depd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.afl3depd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
